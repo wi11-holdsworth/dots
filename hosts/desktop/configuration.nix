14467018b11080b6afeb4cab423469f5f8ceaddd
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-04-02 09:52:34 +1100

    fix perl locales

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -48,15 +48,21 @@
   i18n = {
     defaultLocale = "en_AU.UTF-8";
     extraLocaleSettings = {
+      LANGUAGE = "en_AU.UTF-8";
+      LC_ALL = "en_AU.UTF-8";
+      LC_CTYPE = "en_AU.UTF-8";
+      LC_NUMERIC = "en_AU.UTF-8";
+      LC_COLLATE = "en_AU.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
+      LC_MESSAGES = "en_AU.UTF-8";
+      LC_MONETARY = "en_AU.UTF-8";
       LC_ADDRESS = "en_AU.UTF-8";
       LC_IDENTIFICATION = "en_AU.UTF-8";
       LC_MEASUREMENT = "en_AU.UTF-8";
-      LC_MONETARY = "en_AU.UTF-8";
-      LC_NAME = "en_AU.UTF-8";
-      LC_NUMERIC = "en_AU.UTF-8";
       LC_PAPER = "en_AU.UTF-8";
       LC_TELEPHONE = "en_AU.UTF-8";
-      LC_TIME = "en_AU.UTF-8";
+      LC_NAME = "en_AU.UTF-8";
+      LANG = "en_AU.UTF-8";
     };
   };
 
