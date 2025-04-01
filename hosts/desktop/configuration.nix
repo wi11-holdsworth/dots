582ef45cf62a6863b7a40b8620702aabdb3c8b9f
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-04-02 10:29:06 +1100

    restore changes made in 14467018b11080b6afeb4cab423469f5f8ceaddd and format document

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,14 +1,11 @@
-{
-  pkgs,
-  inputs,
-  ...
+{ pkgs, inputs, ...
 
 }: {
   imports = [
-      ./hardware-configuration.nix
-      ../../modules/nixos/default-desktop.nix
-      inputs.home-manager.nixosModules.home-manager
-    ];
+    ./hardware-configuration.nix
+    ../../modules/nixos/default-desktop.nix
+    inputs.home-manager.nixosModules.home-manager
+  ];
 
   # gaming
   programs = {
@@ -36,7 +33,8 @@
     kernelPackages = pkgs.linuxPackages_testing;
     initrd = {
       kernelModules = [ "amdgpu" ];
-      luks.devices."luks-b164af31-c1c3-4b4e-83c8-eb39802c2027".device = "/dev/disk/by-uuid/b164af31-c1c3-4b4e-83c8-eb39802c2027";
+      luks.devices."luks-b164af31-c1c3-4b4e-83c8-eb39802c2027".device =
+        "/dev/disk/by-uuid/b164af31-c1c3-4b4e-83c8-eb39802c2027";
     };
   };
 
@@ -47,65 +45,43 @@
   };
 
   time.timeZone = "Australia/Melbourne";
-
   i18n = {
     defaultLocale = "en_AU.UTF-8";
-    extraLocaleSettings = {
-      LANGUAGE = "en_AU.UTF-8";
-      LC_ALL = "en_AU.UTF-8";
-      LC_CTYPE = "en_AU.UTF-8";
-      LC_NUMERIC = "en_AU.UTF-8";
-      LC_COLLATE = "en_AU.UTF-8";
-      LC_TIME = "en_GB.UTF-8";
-      LC_MESSAGES = "en_AU.UTF-8";
-      LC_MONETARY = "en_AU.UTF-8";
-      LC_ADDRESS = "en_AU.UTF-8";
-      LC_IDENTIFICATION = "en_AU.UTF-8";
-      LC_MEASUREMENT = "en_AU.UTF-8";
-      LC_PAPER = "en_AU.UTF-8";
-      LC_TELEPHONE = "en_AU.UTF-8";
-      LC_NAME = "en_AU.UTF-8";
-      LANG = "en_AU.UTF-8";
-    };
+    supportedLocales = [ "en_US.UTF-8/UTF-8" "en_AU.UTF-8/UTF-8" ];
+    extraLocaleSettings.LC_ALL = "en_AU.UTF-8";
   };
 
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono 
-  ];
- 
-  environment.systemPackages = with pkgs; [
-    brave
-    ghostty
-    kiwix
-    obsidian
-    signal-desktop
+  fonts.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
 
-    # dev    
-    gh
-    nixfmt
-    vscode
+  environment.systemPackages = with pkgs;
+    [
+      brave
+      ghostty
+      kiwix
+      obsidian
+      signal-desktop
 
-    # gaming
-    mangohud
-    protonup-qt
-    lutris
-    heroic
+      # dev    
+      gh
+      nixfmt
+      vscode
 
-    # uutils
-    bat
-    dust
-    delta
-    eza
-    fd
-    nom
-    ripgrep-all
-    zellij
-  ] ++ (with pkgs.kdePackages; [
-    dragon
-    ktorrent
-    calligra
-    kzones
-  ]);
+      # gaming
+      mangohud
+      protonup-qt
+      lutris
+      heroic
+
+      # uutils
+      bat
+      dust
+      delta
+      eza
+      fd
+      nom
+      ripgrep-all
+      zellij
+    ] ++ (with pkgs.kdePackages; [ dragon ktorrent calligra kzones ]);
 
   services = {
     displayManager.sddm = {
@@ -130,9 +106,7 @@
     graphics = {
       enable = true;
       enable32Bit = true;
-      extraPackages = with pkgs; [
-        amdvlk
-      ];
+      extraPackages = with pkgs; [ amdvlk ];
     };
   };
 
@@ -148,10 +122,7 @@
   };
 
   nix = {
-    settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+    settings.experimental-features = [ "nix-command" "flakes" ];
 
     optimise.automatic = true;
   };
