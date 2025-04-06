197c7e85aaf80f57b33cab4c4a066ec973b06d2b
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-04-07 02:34:12 +1000

    update module template to reflect new style

--- modules/~.nix
+++ modules/~.nix
@@ -1,6 +1,12 @@
-{
-  ...
+{ config, lib }:
+let
+  feature = "feature";
+  cfg = config.${feature};
 
-}: {
+in {
+  options.${feature}.enable = lib.mkEnableOption "enables ${feature}";
 
+  config = lib.mkIf cfg.enable {
+
+  };
 }
