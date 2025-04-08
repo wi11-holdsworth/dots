5847c4da5380c38238d0f71b89127949bac8b97b
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-04-08 14:33:55 +1000

    remove locale warning

--- modules/nixos/features/core.nix
+++ modules/nixos/features/core.nix
@@ -18,7 +18,6 @@ in {
 
     i18n = {
       defaultLocale = "en_AU.UTF-8";
-      extraLocaleSettings.LC_ALL = "en_AU.UTF-8";
       supportedLocales = [ "en_US.UTF-8/UTF-8" "en_AU.UTF-8/UTF-8" ];
     };
 
