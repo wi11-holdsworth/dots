6fec51d560aea6cccdab45c68ce967e60161b7ce
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-04-08 20:39:51 +1000

    add will public key to agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,18 @@
 let
   srv =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOeV0NxqIGIXXgLYE6ntkHE4PARceZBp1FTI7kKLBbk8";
+  will =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPHAnTQP77HQ/8nbf1oX7xftfKYtbH6MSh83wic0qdBy";
+  users = [ srv will ];
 
 in {
-  "borgbackup-server-onsite.age".publicKeys = [ srv ];
-  "borgbackup-server-offsite.age".publicKeys = [ srv ];
-  "api-porkbun.age".publicKeys = [ srv ];
-  "davis-app.age".publicKeys = [ srv ];
-  "davis-admin-password.age".publicKeys = [ srv ];
-  "api-mailjet.age".publicKeys = [ srv ];
-  "secret-mailjet.age".publicKeys = [ srv ];
-  "vaultwarden-admin.age".publicKeys = [ srv ];
-  "aria2.age".publicKeys = [ srv ];
+  "borgbackup-server-onsite.age".publicKeys = users;
+  "borgbackup-server-offsite.age".publicKeys = users;
+  "api-porkbun.age".publicKeys = users;
+  "davis-app.age".publicKeys = users;
+  "davis-admin-password.age".publicKeys = users;
+  "api-mailjet.age".publicKeys = users;
+  "secret-mailjet.age".publicKeys = users;
+  "vaultwarden-admin.age".publicKeys = users;
+  "aria2.age".publicKeys = users;
 }
