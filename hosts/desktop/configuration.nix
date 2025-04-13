9a6c654b3885ad91fabb8fedad4ebdf7626ce97e
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-04-13 23:53:41 +1000

    undo d6df70b28403df6a63b5436e5011502393b28684

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -29,6 +29,8 @@
 
   system.stateVersion = "24.11";
 
+  i18n.extraLocaleSettings.LC_ALL = "en_AU.UTF-8";
+
   users.users.${userName} = {
     extraGroups = [ "networkmanager" "wheel" "scanner" "lp" ];
     isNormalUser = true;
