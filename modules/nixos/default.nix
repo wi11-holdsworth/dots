9e9e4fdea5f8bda792cc7cadf4a70cedcaccd856
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-05-27 18:43:33 +1000

    install agenix by default

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -6,6 +6,7 @@
     in
     map (name: featuresDir + "/${name}") (builtins.attrNames (builtins.readDir featuresDir));
 
+  agenix.enable = true;
   cli-utils.enable = lib.mkDefault true;
   core.enable = lib.mkDefault true;
   direnv.enable = lib.mkDefault true;
