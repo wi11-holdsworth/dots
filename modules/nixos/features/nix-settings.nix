32f04d5e4781eff7f6a6b5bdb102bb868ac49088
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-09-03 17:26:02 +1000

    add automatic nix store garbage collection

--- modules/nixos/features/nix-settings.nix
+++ modules/nixos/features/nix-settings.nix
@@ -5,7 +5,16 @@ in
 {
   config = lib.mkIf config.${feature}.enable {
     nix = {
-      optimise.automatic = true;
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 20d";
+        persistent = true;
+      };
+      optimise = {
+        automatic = true;
+        persistent = true;
+      };
       settings = {
         experimental-features = [
           "nix-command"
