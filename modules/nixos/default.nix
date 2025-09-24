0daeddf190c82c050e34f2e0ccb287ee7d17fed0
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-09-24 19:17:02 +1000

    feat: install howdoi

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -24,50 +24,53 @@ in
   systemd-boot.enable = lib.mkDefault true;
   tailscale.enable = lib.mkDefault true;
 
-  environment.systemPackages = with pkgs; [
-    # keep-sorted start
-    bottom # top
-    broot # large directory browser
-    choose # cut
-    circumflex # hacker news browsing
-    cointop # crypto ticker
-    ddgr # web search
-    dogdns # dns
-    dua # du
-    duf # df
-    epy # ebook reading
-    fd # find
-    fselect # find with sql syntax
-    fx # json processor and viewer
-    fzf # fuzzy finder
-    glow # markdown viewer
-    gping # pretty ping
-    grex # regular expression generator
-    hexyl # hexadecimal viewer
-    hyperfine # benchmarking tool
-    keep-sorted # alphabetical formatter
-    lazygit # git tui
-    mprocs # run long running commands and monitor output
-    navi # cheatsheet browser
-    nb # note taking
-    nixfmt-rfc-style # nix file formatting
-    nom # stylistic nix dependency graphs
-    pastel # colour generation
-    pdd # datetime calculations
-    pirate-get # torrenting
-    procs # ps
-    rates # currency conversion
-    ripgrep-all # grep
-    ripunzip # unzip
-    sd # sed
-    slides # presentations
-    ticker # stock ticker
-    tldr # cheat sheets
-    tmpmail # temporary email address
-    tt # typing test
-    wtfutil # terminal homepage
-    xh # curl
-    xxh # use shell config in remote sessions
-    # keep-sorted end
-  ];
+  environment.systemPackages =
+    with pkgs;
+    [
+      # keep-sorted start
+      bottom # top
+      broot # large directory browser
+      choose # cut
+      circumflex # hacker news browsing
+      cointop # crypto ticker
+      ddgr # web search
+      dogdns # dns
+      dua # du
+      duf # df
+      epy # ebook reading
+      fd # find
+      fselect # find with sql syntax
+      fx # json processor and viewer
+      fzf # fuzzy finder
+      glow # markdown viewer
+      gping # pretty ping
+      grex # regular expression generator
+      hexyl # hexadecimal viewer
+      hyperfine # benchmarking tool
+      keep-sorted # alphabetical formatter
+      lazygit # git tui
+      mprocs # run long running commands and monitor output
+      navi # cheatsheet browser
+      nb # note taking
+      nixfmt-rfc-style # nix file formatting
+      nom # stylistic nix dependency graphs
+      pastel # colour generation
+      pdd # datetime calculations
+      pirate-get # torrenting
+      procs # ps
+      rates # currency conversion
+      ripgrep-all # grep
+      ripunzip # unzip
+      sd # sed
+      slides # presentations
+      ticker # stock ticker
+      tldr # cheat sheets
+      tmpmail # temporary email address
+      tt # typing test
+      wtfutil # terminal homepage
+      xh # curl
+      xxh # use shell config in remote sessions
+      # keep-sorted end
+    ]
+    ++ (with pkgs.python312Packages; [ howdoi ]);
 }
