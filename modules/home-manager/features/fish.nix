f2fa01191b1015182f899bec2d624d02ac01d6a5
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-10-01 14:58:10 +1000

    fix: ensure that programs installed via direnv are detectable by fish-completion

--- modules/home-manager/features/fish.nix
+++ modules/home-manager/features/fish.nix
@@ -39,6 +39,20 @@ in
         vim = "nvim";
         # keep-sorted end
       };
+      plugins = [
+        # INFO: Using this to get shell completion for programs added to the path through nix+direnv.
+        # Issue to upstream into direnv:Add commentMore actions
+        # https://github.com/direnv/direnv/issues/443
+        {
+          name = "completion-sync";
+          src = pkgs.fetchFromGitHub {
+            owner = "iynaix";
+            repo = "fish-completion-sync";
+            rev = "4f058ad2986727a5f510e757bc82cbbfca4596f0";
+            sha256 = "sha256-kHpdCQdYcpvi9EFM/uZXv93mZqlk1zCi2DRhWaDyK5g=";
+          };
+        }
+      ];
     };
 
     # https://nixos.wiki/wiki/Fish#Setting_fish_as_your_shell
