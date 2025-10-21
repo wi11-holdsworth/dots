8f4041dd684db2a2ac5fe1c730ee898520621c4c
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-10-22 01:33:02 +1100

    build: 'amdvlk' has been removed since it was deprecated by AMD. Its replacement, RADV, is enabled by default.

--- modules/nixos/features/amd-gpu.nix
+++ modules/nixos/features/amd-gpu.nix
@@ -1,15 +1,10 @@
 {
-  pkgs,
-  ...
-}:
-{
   # load graphics drivers before anything else
   boot.initrd.kernelModules = [ "amdgpu" ];
 
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    extraPackages = with pkgs; [ amdvlk ];
   };
 
   services.xserver.videoDrivers = [ "amdgpu" ];
