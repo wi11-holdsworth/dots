2d9b057c3a3c57953e02fe517276dbfdcfae7ec5
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-10-22 01:55:04 +1100

    build: git settings have been tidied up

--- modules/home-manager/features/git.nix
+++ modules/home-manager/features/git.nix
@@ -1,44 +1,35 @@
 {
   programs.git = {
     enable = true;
-
-    delta = {
-      enable = true;
-      options.theme = "Dracula";
-    };
-
-    userName = "wi11-holdsworth";
-    userEmail = "83637728+wi11-holdsworth@users.noreply.github.com";
-
-    aliases = {
-      # keep-sorted start
-      a = "add";
-      aa = "add .";
-      ap = "add -p";
-      c = "commit --verbose";
-      ca = "commit -a --verbose";
-      cam = "commit -a -m";
-      cm = "commit -m";
-      co = "checkout";
-      cob = "checkout -b";
-      d = "diff";
-      dc = "diff --cached";
-      ds = "diff --stat";
-      m = "commit --amend --verbose";
-      pl = "pull";
-      ps = "push";
-      s = "status -s";
-      # keep-sorted end
-    };
-
-    extraConfig = {
+    settings = {
       init.defaultBranch = "main";
-
       core.editor = "nvim";
-
       push.autoSetupRemote = true;
-
       pull.rebase = false;
+      user = {
+        name = "wi11-holdsworth";
+        email = "83637728+wi11-holdsworth@users.noreply.github.com";
+      };
+      aliases = {
+        # keep-sorted start
+        a = "add";
+        aa = "add .";
+        ap = "add -p";
+        c = "commit --verbose";
+        ca = "commit -a --verbose";
+        cam = "commit -a -m";
+        cm = "commit -m";
+        co = "checkout";
+        cob = "checkout -b";
+        d = "diff";
+        dc = "diff --cached";
+        ds = "diff --stat";
+        m = "commit --amend --verbose";
+        pl = "pull";
+        ps = "push";
+        s = "status -s";
+        # keep-sorted end
+      };
     };
   };
 }
