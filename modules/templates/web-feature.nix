c9c81b403690780a205c3670a6adecc682ad701a
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-12-29 14:08:20 +1100

    fix: web-feature template now includes a more fleshed-out backup section

--- modules/templates/web-feature.nix
+++ modules/templates/web-feature.nix
@@ -7,7 +7,17 @@ in
       enable = true;
     };
 
-    borgbackup.jobs = feature { };
+    # borgmatic.settings = {
+    #   source_directories = [ ];
+    #   postgresql_databases = [
+    #     {
+    #       name = "feature";
+    #       hostname = "localhost";
+    #       username = "root";
+    #       password = "{credential systemd borgmatic-pg}";
+    #     }
+    #   ];
+    # };
 
     nginx.virtualHosts."feature.fi33.buzz" = {
       forceSSL = true;
