fa31cbd29da94436fca608597b219c0edf561df9
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2026-01-14 12:22:28 +1100

    refactor(fish): prefix aliases that replace a command with a comma

--- modules/home-manager/features/fish.nix
+++ modules/home-manager/features/fish.nix
@@ -11,26 +11,27 @@
     '';
     shellAliases = {
       # keep-sorted start
-      cat = "bat";
-      # cd = "j";
-      cut = "choose";
-      df = "duf";
-      du = "dua";
-      # find = "fd";
-      g = "lazygit";
-      l = "eza";
-      la = "eza -a";
-      ls = "eza";
-      ns = "nh os switch";
-      # curl = "xh";
-      ping = "gping";
-      ps = "procs";
-      # sed = "sd";
-      # grep = "rga";
-      top = "btm";
-      unzip = "ripunzip";
-      vi = "nvim";
-      vim = "nvim";
+      ",cat" = "bat";
+      ",curl" = "xh";
+      ",cut" = "choose";
+      ",df" = "duf";
+      ",diff" = "delta";
+      ",du" = "dua";
+      ",find" = "fd";
+      ",grep" = "rga";
+      ",ping" = "gping";
+      ",ps" = "procs";
+      ",sed" = "sd";
+      ",ss" = "snitch";
+      ",top" = "btm";
+      ",unzip" = "ripunzip";
+      "g" = "lazygit";
+      "l" = "eza";
+      "la" = "eza -a";
+      "ls" = "eza";
+      "ns" = "nh os switch";
+      "vi" = "nvim";
+      "vim" = "nvim";
       # keep-sorted end
     };
     plugins = [
